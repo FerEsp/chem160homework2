d711e47745a035080996a6ff3fcaa5a7198ce04f
FerEsp <70300245+FerEsp@users.noreply.github.com>
2020-09-10 13:48:00 -0700

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,19 @@
+from random import choices
+ntrials = 15000
+player1wins = 0
+ndice1 = 3
+ndice2 = 2
+for i in range(ntrials):
+    dice2 = choices(range(1,7), k = ndice2)
+    if dice2[0] == dice2[1]:
+        continue
+    dice1 = choices(range(1,7), k = ndice1)
+    dice1.sort(reverse = True)
+    dice2.sort(reverse = True)
+    sum1 = dice1[0] + dice1[1]
+    sum2 = dice2[0] + dice2[1]
+    if sum1 > sum2:
+        player1wins += 1
+print("Player 1's win percentage =" , player1wins/ntrials)
+
+# It's a fair game because there win percentages is about 50/50.
